4c173b3fcb00567378b4cf326cadbca047d2e7e6
bm4656 <newjin46@gmail.com>
2024-12-27 18:12:34 +0900

    feat: Posts CRUD 구현

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,130 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 1000000,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '노래 연습하고 있는 해린',
+    likeCount: 1000000,
+    commentCount: 999999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '종합운동장에서 운동중인 로제',
+    likeCount: 1000000,
+    commentCount: 999999,
+  },
+];
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  // 1. GET /posts -> 모든 포스트를 조회한다.
+  @Get()
+  getPosts() {
+    return posts;
+  }
+
+  // 2. GET /posts/:id -> id에 해당하는 포스트를 가져온다.
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException(`데이터가 존재하지 않습니다.(${id})`);
+    }
+
+    return post;
+  }
+
+  // 3. POST /posts -> 새로운 포스트를 생성한다.
+  @Post()
+  postPosts(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    const post = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts = [...posts, post];
+    return post;
+  }
+
+  // 4. PUT /posts/:id -> id에 해당하는 포스트를 수정한다.
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException(`데이터가 존재하지 않습니다.(${id})`);
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    posts.map((prevPost) => (prevPost.id === Number(id) ? post : prevPost));
+    return posts;
+  }
+
+  // 5. DELETE /posts/:id -> id에 해당하는 포스트를 삭제한다.
+  @Delete(':id')
+  deletePost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === Number(id));
+    if (!post) {
+      throw new NotFoundException(`데이터가 존재하지 않습니다.(${id})`);
+    }
+
+    posts = posts.filter((post) => post.id !== Number(id));
+    return id;
+  }
+}
