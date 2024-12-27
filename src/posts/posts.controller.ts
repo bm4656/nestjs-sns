a1880f1ba335b79b63a4b7dbd7ba4544bbc8d184
bm4656 <newjin46@gmail.com>
2024-12-27 18:45:55 +0900

    refactor: service로 로직 이동
    서비스 레이어 분리
    - 모든 비즈니스 로직은 서비스 레이어에 존재
    - 컨트롤러 레이어는 서비스 레이어의 메서드를 사용

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,48 +3,11 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
-
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_official',
-    title: '뉴진스 민지',
-    content: '메이크업 고치고 있는 민지',
-    likeCount: 1000000,
-    commentCount: 999999,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '뉴진스 해린',
-    content: '노래 연습하고 있는 해린',
-    likeCount: 1000000,
-    commentCount: 999999,
-  },
-  {
-    id: 3,
-    author: 'blackpink_official',
-    title: '블랙핑크 로제',
-    content: '종합운동장에서 운동중인 로제',
-    likeCount: 1000000,
-    commentCount: 999999,
-  },
-];
 
 @Controller('posts')
 export class PostsController {
@@ -53,18 +16,13 @@ export class PostsController {
   // 1. GET /posts -> 모든 포스트를 조회한다.
   @Get()
   getPosts() {
-    return posts;
+    return this.postsService.getAllPosts();
   }
 
   // 2. GET /posts/:id -> id에 해당하는 포스트를 가져온다.
   @Get(':id')
   getPost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException(`데이터가 존재하지 않습니다.(${id})`);
-    }
-
-    return post;
+    return this.postsService.getPostById(Number(id));
   }
 
   // 3. POST /posts -> 새로운 포스트를 생성한다.
@@ -74,17 +32,7 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const post = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-
-    posts = [...posts, post];
-    return post;
+    return this.postsService.createPost(author, title, content);
   }
 
   // 4. PUT /posts/:id -> id에 해당하는 포스트를 수정한다.
@@ -95,36 +43,12 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException(`데이터가 존재하지 않습니다.(${id})`);
-    }
-
-    if (author) {
-      post.author = author;
-    }
-
-    if (title) {
-      post.title = title;
-    }
-
-    if (content) {
-      post.content = content;
-    }
-
-    posts.map((prevPost) => (prevPost.id === Number(id) ? post : prevPost));
-    return posts;
+    return this.postsService.updatePost(Number(id), author, title, content);
   }
 
   // 5. DELETE /posts/:id -> id에 해당하는 포스트를 삭제한다.
   @Delete(':id')
   deletePost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === Number(id));
-    if (!post) {
-      throw new NotFoundException(`데이터가 존재하지 않습니다.(${id})`);
-    }
-
-    posts = posts.filter((post) => post.id !== Number(id));
-    return id;
+    return this.postsService.deletePost(Number(id));
   }
 }
