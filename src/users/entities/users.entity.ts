b7661918f21532355d43fd62b76199bd33126063
bm4656 <newjin46@gmail.com>
2025-01-11 11:30:16 +0900

    feat: Author Relation 생성

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { RolesEnum } from '../const/roles.enum';
+import { PostsModel } from '../../posts/entities/posts.entity';
 
 @Entity()
 export class UsersModel {
@@ -27,4 +28,7 @@ export class UsersModel {
     default: RolesEnum.USER,
   })
   role: RolesEnum;
+
+  @OneToMany(() => PostsModel, (post) => post.author)
+  posts: PostsModel[];
 }
