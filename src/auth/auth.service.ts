4f06cbbbc8d0b76313ad9f2ca5a350d6dc86a84e
bm4656 <newjin46@gmail.com>
2025-01-15 01:43:19 +0900

    feat: auth 서비스 정의 및 signToken 구현

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModel } from '../users/entities/users.entity';
+import { JWT_SECRET } from './const/auth.const';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  /**
+   * 우리가 만드려는 기능
+   *
+   * 1) registerWithEmail
+   *    - email, nickname, password를 입력받고 사용자를 생성한다.
+   *    - 생성이 완료되면 accessToken과 refreshToken을 반환한다.
+   *      회원가입 후 다시 로그인해주세요 <- 이런 쓸데없는 과정 방지하기 위해서
+   *
+   * 2) loginWithEmail
+   *    - email, password를 입력하면 사용자 검증을 진행한다.
+   *    - 검증이 완료되면 accessToken과 refreshToken을 반환한다.
+   *
+   * 3) loginUser
+   *    - (1)과 (2)에서 필요한 accessToken과 refreshToken을 반환하는 로직
+   *
+   * 4) signToken
+   *    - (3)에서 필요한 accessToken과 refreshToken을 sign 로직
+   *    - 즉, 토큰을 생성하는 로직
+   *
+   * 5) authenticateWithEmailAndPassword
+   *    - (2)에서 로그인을 진행할 때 필요한 기본적인 검증 진행
+   *      1. 사용자가 존재하는지 확인 (email)
+   *      2. 비밀번호가 맞는지 확인
+   *      3. 모두 통과되면 찾은 사용자 정보 반환
+   *      4. (3)으로 가서 loginWithEmail에서 반환된 데이터를 기반으로 토큰 생성
+   *    - (2)에서 로직을 다 써도 되는데 분리한 것
+   */
+
+  /**
+   * Payload에 들어갈 정보
+   *
+   * 1) email
+   * 2) sub -> id
+   * 3) type : 'access' | 'refresh'
+   */
+
+  signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: JWT_SECRET,
+      expiresIn: isRefreshToken ? 3600 : 300,
+    });
+  }
+}
