f244b0cd6b000e98784d9ccc7a181f7d9cc030f0
bm4656 <newjin46@gmail.com>
2025-01-18 00:30:02 +0900

    feat: 로그인 시 토큰 시스템 사용하도록 적용

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -19,7 +19,7 @@ export class AuthService {
    * {authorization: ‘Basic {token}’}
    * {authorization: ‘Bearer {token}’}
    */
-  async extractTokenFromHeader(header: string, isBearer: boolean) {
+  extractTokenFromHeader(header: string, isBearer: boolean) {
     const splitToken = header.split(' ');
 
     const prefix = isBearer ? 'Bearer' : 'Basic';
@@ -29,6 +29,31 @@ export class AuthService {
     }
 
     return splitToken[1];
+  }
+
+  /**
+   * Basic adafkjnan;efienfeafyyajaakjn
+   *
+   * 1) adafkjnan;efienfeafyyajaakjn -> email:password
+   * 2) email:password -> ['email', 'password']
+   * 3) {email: email, password: password}
+   */
+  decodeBasicToken(base64String: string) {
+    const decoded = Buffer.from(base64String, 'base64').toString('utf-8');
+
+    const split = decoded.split(':');
+
+    if (split.length !== 2) {
+      throw new UnauthorizedException('잘못된 유형의 토큰입니다!');
+    }
+
+    const email = split[0];
+    const password = split[1];
+
+    return {
+      email,
+      password,
+    };
   }
 
   /**
